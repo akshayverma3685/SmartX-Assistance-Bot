680bf0df03064525c8c4b353b855f4ca2dbaafe1
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-28 12:00:30 +0530

    Create __init__.py

---
+++ keyboards/__init__.py
@@ -0,0 +1,26 @@
+# keyboards/__init__.py
+"""
+Keyboard exports for easy imports.
+
+Usage:
+    from keyboards import main_menu_kb, ai_menu_kb, downloader_menu_kb, ...
+"""
+from .main_menu import main_menu_kb
+from .ai_menu import ai_menu_kb
+from .premium_menu import premium_menu_kb
+from .downloader_menu import downloader_menu_kb
+from .tools_menu import tools_menu_kb
+from .business_menu import business_menu_kb
+from .entertainment_menu import entertainment_menu_kb
+from .admin_menu import admin_menu_kb
+
+__all__ = [
+    "main_menu_kb",
+    "ai_menu_kb",
+    "premium_menu_kb",
+    "downloader_menu_kb",
+    "tools_menu_kb",
+    "business_menu_kb",
+    "entertainment_menu_kb",
+    "admin_menu_kb",
+]

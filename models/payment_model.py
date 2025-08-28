80c6149d8212754c4964dc21131f8452176ba273
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-28 11:22:02 +0530

    Update payment_model.py

--- models/payment_model.py
+++ models/payment_model.py
@@ -1,1 +1,14 @@
+# models/payment_model.py
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
 
+class PaymentModel(BaseModel):
+    payment_id: str
+    user_id: int
+    amount: float
+    currency: str = "INR"
+    method: str
+    status: str
+    plan_duration_days: Optional[int]
+    date: datetime = datetime.utcnow()

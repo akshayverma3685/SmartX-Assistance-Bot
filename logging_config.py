02b7bab3ea713240a18ae14d924d55c3e5ac574d
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-28 11:55:16 +0530

    Update logging_config.py

--- logging_config.py
+++ logging_config.py
@@ -16,11 +16,14 @@ def setup_logging(level="INFO"):
     ch.setFormatter(fmt)
     logger.addHandler(ch)
 
-    fh = RotatingFileHandler(os.path.join(LOG_DIR, "bot.log"), maxBytes=5*1024*1024, backupCount=5)
+    fh = RotatingFileHandler(os.path.join(LOG_DIR, "bot.log"), maxBytes=10*1024*1024, backupCount=5)
     fh.setFormatter(fmt)
     logger.addHandler(fh)
 
-    # payments log
-    ph = RotatingFileHandler(os.path.join(LOG_DIR, "payments.log"), maxBytes=2*1024*1024, backupCount=3)
+    ph = RotatingFileHandler(os.path.join(LOG_DIR, "payments.log"), maxBytes=5*1024*1024, backupCount=3)
     ph.setFormatter(fmt)
     logging.getLogger("smartx_bot.payment_service").addHandler(ph)
+
+    eh = RotatingFileHandler(os.path.join(LOG_DIR, "errors.log"), maxBytes=5*1024*1024, backupCount=3)
+    eh.setFormatter(fmt)
+    logging.getLogger("smartx_bot").addHandler(eh)
